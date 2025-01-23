2750931ffd34363763b9d2b0d1177bdf550e45ac
Aman Agarwal <agarwalaman190202@gmail.com>
2025-01-22 21:26:38 -0500

    Patchify function added

---
+++ main.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+from tqdm import tqdm, trange
+
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+
+from torchvision.transforms import ToTensor
+from torchvision.datasets.mnist import MNIST
+
+np.random.seed(0)
+torch.manual_seed(0)
+
+def patchify(images, n_patches):
+    n, c, h, w = images.shape
+
+    assert h == w, "Patchify method is implemented for square images only"
+
+    patches = torch.zeros(n, n_patches ** 2, h * w * c // n_patches ** 2)
+    patch_size = h // n_patches
+
+    for idx, image in enumerate(images):
+        for i in range(n_patches):
+            for j in range(n_patches):
+                patch = image[:, i * patch_size: (i + 1) * patch_size, j * patch_size: (j + 1) * patch_size]
+                patches[idx, i * n_patches + j] = patch.flatten()
+    return patches
+
+class myViT(nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self,images):
+        pass
+
+def main():
+    transform = ToTensor()
+
+    train_set = MNIST(root='./../datasets', train=True, download=True, transform=transform)
+    test_set = MNIST(root='./../datasets', train=False, download=True, transform=transform)
+
+    train_loader = DataLoader(train_set, shuffle=True, batch_size=128)
+    test_loader = DataLoader(test_set, shuffle=False, batch_size=128)
+
+    device = ("cuda" if torch.cuda.is_available() else "mps") 
+    print("Using device {}")
+
+
+if __name__ == '__main__':
+    main()
+
